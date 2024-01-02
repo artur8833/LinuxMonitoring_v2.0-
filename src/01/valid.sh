f4895f08047017bc80bf37d7decbd16289e29bc0
Your Name <yronwode@su-b2.surgut.21-school.ru>
2024-01-02 21:06:57 +0500

    Start scripts

---
+++ src/01/valid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d "$absolut_path" ]
+then 
+    echo "No such directory" 
+elif [[ ! "$2" =~ ^[0-9]+$ ]]
+then 
+    echo "параметр 2 должен быть числом"
+elif [  ]
+then
+
+fi
