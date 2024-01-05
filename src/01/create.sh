4296e3f292fe16215da2a6b6c23cb2c2e5d7d116
Your Name <yronwode@su-a1.surgut.21-school.ru>
2024-01-05 20:20:44 +0500

    Add create folders

--- src/01/create.sh
+++ src/01/create.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
+filename=$3
 len_filename=$(echo -n "$3" | wc -m)
-echo ${ "$3": -1 }
+date=$(date "+%d%m%y")
 
-# for (( i=1; i <= $2; i++ ))
-# do
-#     if (len_filename)
+start_position=1
+last_position=1
 
-# done
+echo "${filename:0:1}${filename:0:1}${filename:1:len_filename}"
+
+
+while (($len_filename<4))
+do
+    filename="$filename${filename: -1}"
+    ((len_filename++))
+done
+
+echo "filename==$filename, len_filename==$len_filename"
+
+for (( i=1; i<=$2 ; i++ ))
+do
+    if (($len_filename>116))
+    then 
+        ((j++))
+        len_filename=$(echo -n "$3" | wc -m)
+        filename=$3
+        ((middle_position++))
+
+        if((j>1))
+        then 
+            ((start_position++))
+            ((last_position++))
+        fi
+    fi
+    
+    mkdir "$filename"_"$date" ; cd "$filename"_"$date"
+    filename="${filename:0:start_position}${filename:middle_position:1}${filename:last_position:len_filename}"
+    ((len_filename++))
+    echo "$i"
+done
