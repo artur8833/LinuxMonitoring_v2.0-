aa2bcca5e1fa2585250b3a6b427978a646f48968
Artur <arturgazizov@MacBook-Air-Artur.local>
2024-01-25 22:33:17 +0500

    Refactor

--- src/03/delete_by_pattern.sh
+++ src/03/delete_by_pattern.sh
@@ -10,9 +10,3 @@ do
     find $files -exec rm -rf {} + 2>/dev/null
 
 done
-
-# find / -regextype posix-extended -regex './^[a-zA-Z]_[0-9]{6}' 2>/dev/null 
-
-# mapfile -t all_path < <(find / -type d -name "*_24[0-9][0-9][0-9][0-9]")
-
-# find / -type d -regex '_[0-9]{6}$' 2>/dev/null >> pattern.txt
