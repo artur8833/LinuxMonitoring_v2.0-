4c2f41ee660c31dbe2824aa13fb09b5d4e520b81
Artur <arturgazizov@MacBook-Air-Artur.local>
2024-01-27 19:16:24 +0500

    add 4

--- src/03/delete_by_pattern.sh
+++ src/03/delete_by_pattern.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Start find by pattern"
 
-
 mapfile -t pattern_path < <(find / -name "*_250124" 2>/dev/null)
 
 for files in "${pattern_path[@]}"
--- src/04/main.sh
+++ src/04/main.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-codes=(200 201 400 401 403 404 500 501 502 500)
-metods=(GET POST PUT PATCH DELETE)
+codes=("200" "201" "400" "401" "403" "404" "500" "501" "502" "500")
+metods=("GET" "POST" "PUT" "PATCH" "DELETE")
+agents=("Mozilla" "Google" "Chrome" "Opera" "Safari" "Internet Explorer" "Microsoft Edge" "Crawler and bot" "Library and net tool")
 
-
-for ((days=1;days<=5;days++))
+for ((days=0;days<=4;days++))
 do
+    hours=$(shuf -i 0-24 -n1)
+    dates=$(date +%d/%m/%Y: -d "$days days")
+    times=$(date +:%H:%M:%S)
     file_name="log_file_$days"
-    echo "$file_name"
     maximum_line=$(shuf -i 100-1000 -n1)
-    echo "$maximum_line" 
     for ((current_line=1; current_line < $maximum_line;current_line++))
     do
+        times=$(date +%H:%M:%S -d "$(shuf -i 1-900 -n1) seconds")
         ip=$(shuf -i 0-255 -n4 | tr '\n' '.' | sed 's/.\{1\}$//')
-        echo "$ip ${codes[$(shuf -i 0-9 -n1)]} ${metods[$(shuf -i 0-4 -n1)]}"
+        url="http://www.random.com/$(shuf -i 0-1000 -n1))"
+        echo "$ip - - [$dates$times] \"${metods[$(shuf -i 0-4 -n1)]} $url\" ${codes[$(shuf -i 0-9 -n1)]} ${agents[$(shuf -i 0-8 -n1)]}">>logs_$days.log
     done
 
 done 
